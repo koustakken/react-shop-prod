d8ad7f00a2aee348318144c21fe0bd92868a351e
koustakken <kous.takken@gmail.com>
2024-03-26 12:23:45 +0300

    Временно пропускаем юнит тесты, ошибка с глобальнымим импортами

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import { Button } from './Button'
-// TODO надо исправить
+
 // если не добавлять в каждый файл будет ошибка
+// использования .toBeInTheDocument()
 import '@testing-library/jest-dom'
 
-describe('', () => {
-  test('', () => {
+describe('button unit', () => {
+  test('example', () => {
     // eslint-disable-next-line i18next/no-literal-string
     render(<Button>Test</Button>)
     expect(screen.getByText('Test')).toBeInTheDocument()
-    //  Cannot find module '@/shared/lib/classNames/classNames' from 'src/shared/ui/Button/Button.tsx'
   })
 })
