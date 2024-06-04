6153fc680270725a98e62f757394715f87277aa0
u-003-076 <artem.ivanov@korvet-jsc.ru>
2024-06-04 11:47:32 +0500

    wlcm page

--- src/pages/WelcomePage/ui/WelcomePage.tsx
+++ src/pages/WelcomePage/ui/WelcomePage.tsx
@@ -1,11 +1,28 @@
 import { classNames } from '@/shared/lib/classNames/classNames'
 import styles from './WelcomePage.module.scss'
 import { useTranslation } from 'react-i18next'
+import { Button, ThemeButton } from '@/shared/ui/Button/Button'
+import { Link } from 'react-router-dom'
+
 const WelcomePage = () => {
-  const {t} = useTranslation()
+  const { t } = useTranslation()
   return (
     <div className={classNames(styles.root)}>
       <h1>{t('Welcome Page')}</h1>
+      <span>
+        {t(
+          'WeightHub - training organization, planning and progression and results analysis'
+        )}
+      </span>
+      <div className={styles.btns}>
+        <div className={styles.sign}>
+          <Button theme={ThemeButton.SUBMIT}>
+            <Link to={'/auth'}>{t('Sign In')}</Link>
+          </Button>
+          <Button theme={ThemeButton.EDIT}>{t('Sign Up')}</Button>
+        </div>
+        <Button theme={ThemeButton.OUTLINED}>{t('GitHub')}</Button>
+      </div>
     </div>
   )
 }
